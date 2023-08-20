58c530ce9ee4d3a11e303971b0e663fbb7493971
minhkhoinhmk <minhkhoi11.04nhmk@gmail.com>
2023-08-20 16:31:33 +0700

    Going live

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,17 +14,24 @@ import { configValidationSchema } from './config.schema';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-        autoLoadEntities: true,
-        synchronize: true,
-        entities: [Character],
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const isProductions = configService.get('STAGE') === 'prod';
+        return {
+          ssl: isProductions,
+          extra: {
+            ssl: isProductions ? { rejectUnauthorized: false } : null,
+          },
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          autoLoadEntities: true,
+          synchronize: true,
+          entities: [Character],
+        };
+      },
     }),
     CharacterModule,
   ],
