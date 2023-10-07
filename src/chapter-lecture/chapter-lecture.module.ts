35f80440e08b1f75f35bac5c76ad46b1ff801095
Nguyen Khoi <minhkhoi1.04nhmk@gmail.com>
2023-10-07 15:34:14 +0700

    fix(US00010): fix chapter- lecture

--- src/chapter-lecture/chapter-lecture.module.ts
+++ src/chapter-lecture/chapter-lecture.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChapterLecture } from './entity/chapter-lecture.entity';
 
-@Module({})
+@Module({ imports: [TypeOrmModule.forFeature([ChapterLecture])] })
 export class ChapterLectureModule {}
