0a73bb9ae26e2afdcbb03c35114cdc0e1e5b723e
Vennis-F <hoanganhgo28062001@gmail.com>
2023-11-10 21:25:48 +0700

    Fix(US00038): Fix orderStatus into field

--- src/learner-course/learner-course.service.ts
+++ src/learner-course/learner-course.service.ts
@@ -1,4 +1,9 @@
-import { ConflictException, Injectable, Logger } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
 import { CourseRepository } from 'src/course/course.repository';
 import { LearnerRepository } from 'src/learner/learner.repository';
 import { LearnerCourseRepository } from './learner-course.repository';
@@ -6,6 +11,8 @@ import { CreateLearnerCourseRequest } from './dto/request/create-learner-course.
 import { Learner } from 'src/learner/entity/learner.entity';
 import { Course } from 'src/course/entity/course.entity';
 import { LearnerCourse } from './entity/learner-course.entity';
+import { UpdateLearnerCourseRequest } from './dto/request/update-learner-course.dto';
+import { User } from 'src/user/entity/user.entity';
 
 @Injectable()
 export class LearnerCourseService {
@@ -50,5 +57,119 @@ export class LearnerCourseService {
         `Learner Course with lernerId ${createLearnerCourseRequest.learnerId} and courseId ${createLearnerCourseRequest.courseId} was existed`,
       );
     }
+  }
+
+  async updateLearnerCourse(body: UpdateLearnerCourseRequest) {
+    const { courseId, currentLearnerId, newLearnerId } = body;
+
+    const course: Course = await this.courseRepository.getCourseById(courseId);
+    if (!course) {
+      this.logger.log(
+        `method=updateLearnerCourse, courseId ${courseId} is not existed`,
+      );
+
+      throw new BadRequestException(`courseId ${courseId} is not existed`);
+    }
+
+    // None -> A (chưa tạo)
+    // None -> A (đã tạo)
+    if (!currentLearnerId && newLearnerId) {
+      const newLearner = await this.learnerRepository.getLeanerById(
+        newLearnerId,
+      );
+      const newLearnerCourse =
+        await this.learnerCourseRepository.getLearnerCourseByCourseAndLearner(
+          course,
+          newLearner,
+        );
+
+      if (!newLearnerCourse)
+        await this.learnerCourseRepository.saveLearnerCourse(
+          await this.learnerCourseRepository.createLearnerCourse(
+            newLearner,
+            course,
+          ),
+        );
+      else await this.changeActiveLearnerCourse(newLearnerCourse, true);
+    }
+    //A(Đã tạo) -> None
+    else if (currentLearnerId && !newLearnerId) {
+      const currentLearner = await this.learnerRepository.getLeanerById(
+        currentLearnerId,
+      );
+      const currentLearnerCourse =
+        await this.learnerCourseRepository.getLearnerCourseByCourseAndLearner(
+          course,
+          currentLearner,
+        );
+      await this.changeActiveLearnerCourse(currentLearnerCourse, false);
+    }
+    // A(Đã tạo) -> B(Đã tạo) và B(Đã tạo) -> A(Đã tạo)
+    // A(Đã tạo) -> B(Chưa tạo) và B(Đã tạo) -> A(Đã tạo)
+    else if (currentLearnerId && newLearnerId) {
+      const currentLearner = await this.learnerRepository.getLeanerById(
+        currentLearnerId,
+      );
+      const currentLearnerCourse =
+        await this.learnerCourseRepository.getLearnerCourseByCourseAndLearner(
+          course,
+          currentLearner,
+        );
+      const newLearner = await this.learnerRepository.getLeanerById(
+        newLearnerId,
+      );
+      const newLearnerCourse =
+        await this.learnerCourseRepository.getLearnerCourseByCourseAndLearner(
+          course,
+          newLearner,
+        );
+
+      if (newLearnerCourse) {
+        await this.changeActiveLearnerCourse(currentLearnerCourse, false);
+        await this.changeActiveLearnerCourse(newLearnerCourse, true);
+      } else {
+        await this.changeActiveLearnerCourse(currentLearnerCourse, false);
+        await this.learnerCourseRepository.saveLearnerCourse(
+          await this.learnerCourseRepository.createLearnerCourse(
+            newLearner,
+            course,
+          ),
+        );
+      }
+    }
+  }
+
+  async changeActiveLearnerCourse(
+    learnerCourse: LearnerCourse,
+    active: boolean,
+  ) {
+    learnerCourse.active = active;
+    this.learnerCourseRepository.saveLearnerCourse(learnerCourse);
+  }
+
+  async getLearnerIsLearningCourseByCourseId(courseId: string, user: User) {
+    const learners: Learner[] = await this.learnerRepository.getLearnerByUserId(
+      user.id,
+    );
+    const course: Course = await this.courseRepository.getCourseById(courseId);
+    let learnerId = '';
+
+    const listPromises = [];
+    learners.forEach((learner) => {
+      listPromises.push(
+        this.learnerCourseRepository.getLearnerCourseByCourseAndLearner(
+          course,
+          learner,
+        ),
+      );
+    });
+
+    const learnerCourses: LearnerCourse[] = await Promise.all(listPromises);
+
+    learnerCourses.forEach((learnerCourse) => {
+      if (learnerCourse?.active) learnerId = learnerCourse.learner.id;
+    });
+
+    return { learnerId };
   }
 }
